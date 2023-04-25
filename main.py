9566a3e22153ff7b514a382c653b5dfa47e56a4b
aczo <adam.t.czopek@gmail.com>
2023-04-25 13:09:47 +0200

    Initial version

---
+++ main.py
@@ -0,0 +1,21 @@
+# Prime numbers
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+
+# Data for plotting
+t = np.arange(0.0, 6.0, 0.05)
+s = scipy.special.gamma(t)
+
+# x = np.arange(0, 6, 1)
+# y = np.math.factorial(x)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='x', ylabel='y',
+       title='Gamma vs factorial')
+ax.grid()
+
+plt.show()
