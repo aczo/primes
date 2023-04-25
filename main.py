5231d440d2404dbb5c0d490a964fe91f3188d271
aczo <adam.t.czopek@gmail.com>
2023-04-25 13:49:11 +0200

    Corrected shift - factorial right, gamma unshifted

--- main.py
+++ main.py
@@ -5,19 +5,20 @@ import numpy as np
 import scipy
 
 # Data for plotting
-t = np.arange(0.0, 6.0, 0.05)
-s = scipy.special.gamma(t+1)
+t = np.linspace(-1.0, 6.0, 1000)
+s = scipy.special.gamma(t)
 
-x = np.arange(0, 6, 1)
-y = scipy.special.factorial(x)
+x = np.arange(1, 7, 1)
+y = scipy.special.factorial(x-1)
 
 fig, ax = plt.subplots()
-ax.plot(t, s)
+ax.plot(t, s, label="Gamma")
 
-ax.plot(x, y, "or")
+ax.plot(x, y, "or", label="(x-1)!")
 
 ax.set(xlabel='x', ylabel='y',
        title='Gamma vs factorial')
+ax.legend()
 ax.grid()
 
 plt.show()
