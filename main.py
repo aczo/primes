07616baca56885456f21bd0240d6cc8484511039
aczo <adam.t.czopek@gmail.com>
2023-04-25 13:23:06 +0200

    Added factorial, shifted gamma left to overlay factorial

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import scipy
 
 # Data for plotting
 t = np.arange(0.0, 6.0, 0.05)
-s = scipy.special.gamma(t)
+s = scipy.special.gamma(t+1)
 
-# x = np.arange(0, 6, 1)
-# y = np.math.factorial(x)
+x = np.arange(0, 6, 1)
+y = scipy.special.factorial(x)
 
 fig, ax = plt.subplots()
 ax.plot(t, s)
+
+ax.plot(x, y, "or")
 
 ax.set(xlabel='x', ylabel='y',
        title='Gamma vs factorial')
