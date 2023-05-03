45cfbb5073a31b448755db6f5e4e03af61d15569
aczo <adam.t.czopek@gmail.com>
2023-05-03 17:20:59 +0200

    Corrected progress message text

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from plots import plotPrimeCount
 
 
 for z in range(2):
-    print("Generating plot for " + str(z) + " trivial zeroes...", end="", flush=True)
+    print("Generating plot for " + str(z) + " non-trivial zeroes...", end="", flush=True)
     start = time()
     plt = plotPrimeCount(z)
     print("done. Time elapsed: " + str(timedelta(seconds=time()-start)))
