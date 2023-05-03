e8bb360ef3aa243787d318989f9864d2e2811b50
aczo <adam.t.czopek@gmail.com>
2023-05-03 11:37:31 +0200

    Updated zeta zero corrections based on Hans Riesel's formula. Reformatted plots

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # Main function
 from plots import plotPrimeCount
-from primes import RiemannR, RiemannPi
 
 
-plotPrimeCount()
-# print(RiemannPi(20))
-# print(RiemannR(20))
+plt = plotPrimeCount()
+plt.savefig("test.png")
--- plots.py
+++ plots.py
@@ -1,24 +1,20 @@
-#library of various plots
+# Implementation of various plots
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import gamma, factorial
-import mpmath as mp
 import complexplot as cp
 import primes
 
 
-# vectorize some useful functions
+# vectorize functions that are referred to in plots
 primesbelow = np.vectorize(primes.primesbelow)
-li = np.vectorize(mp.li)
-RiemannR = np.vectorize(primes.RiemannR)
 RiemannPi = np.vectorize(primes.RiemannPi)
+
 
 def plotgamma():                # gamma function display - auxiliary plot
     x = np.linspace(-3.5, 5.5, 2251)
-    # Data for plotting
     y = gamma(x)
-
     plt.plot(x, y, 'b', alpha=0.6, label='gamma(x)')
     k = np.arange(1, 7)
     plt.plot(k, factorial(k-1), 'r*', alpha=0.6, label='(x-1)!, x = 1, 2, ...')
@@ -30,22 +26,40 @@ def plotgamma():                # gamma function display - auxiliary plot
     plt.legend(loc='lower right')
     plt.show()
 
+
+def plotPrimeCount(N = 0):    # prime numbers plot, with N being number of zeta zeros to include in R(x) correction
+    x = np.linspace(1, 100, 3000)
+    y = RiemannPi(x)
+    p = primesbelow(x)
+
+    px = 1 / plt.rcParams['figure.dpi']
+    figure, axis = plt.subplots(1, 2, figsize=(1920*px, 1080*px))
+    figure.suptitle("Approximation of prime counting function by Riemann's pi(x)")
+    plt.subplots_adjust(left=0.03, right=0.97, top=0.9, bottom=0.03, wspace=0.07, hspace=0.05)
+    axis[0].plot(x, y, 'g', label='R(x)')
+    axis[0].plot(x, p, 'r', label='primes')
+    axis[0].set_label('x')
+    axis[0].set_label('y')
+    axis[0].set_title("0 < x < 100")
+    axis[0].set_xlim([0, 100])
+    axis[0].set_ylim([0, 26])
+    axis[0].legend(loc='lower right')
+    axis[1].plot(x, y, 'g', label='R(x)')
+    axis[1].plot(x, p, 'r', label='primes')
+    axis[1].set_label('x')
+    axis[1].set_label('y')
+    axis[1].set_title("10 < x < 20")
+    axis[1].set_xlim([10, 20])
+    axis[1].set_ylim([3.5, 8.5])
+    axis[1].legend(loc='lower right')
+    figure.text(0.43, 0.93, str(N) + " non-trivial Zeta zeros corrections included")
+    return plt
+    # plt.show()
+
+
 def plotSampleC():              # sample complex plot with hsv coloring
     x = np.arange(-1, 4, 0.05)
     y = np.arange(-1, 3, 0.05)
     x, y = np.meshgrid(x, y)
     z = x + 1j * y
     cp.plotComplex(z, [-1, 4, -1, 3])
-
-def plotPrimeCount():    # prime numbers plot
-    x = np.linspace(1, 100, 3000)
-    y = RiemannPi(x)
-    p = primesbelow(x)
-    plt.plot(x, y, 'g', label='R(x)')
-    plt.plot(x, p, 'r', label='primes')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.xlim([0, 100])
-    plt.ylim([0, 26])
-    plt.legend(loc='lower right')
-    plt.show()
