3be69f3bc2bd8857f567e98cb63c970d634cfae7
aczo <adam.t.czopek@gmail.com>
2023-05-04 10:11:25 +0200

    Added command line arguments handling

--- main.py
+++ main.py
@@ -2,9 +2,22 @@
 from time import time
 from datetime import timedelta
 from plots import plotPrimeCount
+import argparse
 
 
-for z in range(1):
+def parse_range(astr):
+    result = set()
+    for part in astr.split(','):
+        x = part.split('-')
+        result.update(range(int(x[0]), int(x[-1]) + 1))
+    return sorted(result)
+
+parser = argparse.ArgumentParser(description = "Generates plots of Riemann R(x) with specified number of zeta zeroes corrections")
+parser.add_argument('zeroes', type=parse_range, help='List of plots with given numbers of zeta zeroes, e.g. 1,2,4-8')
+args = parser.parse_args()
+print(args.zeroes)
+
+for z in args.zeroes:
     print("Generating plot for " + str(z) + " non-trivial zeroes...", end="", flush=True)
     start = time()
     plt = plotPrimeCount(z)
